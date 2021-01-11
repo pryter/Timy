3c57641f3629d0fc56af0877c6c00575c5678fce
Peter <peterphongpak@gmail.com>
2021-01-11 13:47:25 +0700

    Fix timestamp parser

--- src/utilities/numberprocessing.ts
+++ src/utilities/numberprocessing.ts
@@ -1,11 +1,11 @@
 export const convertTime = (ms: number) => {
     let millis: number, seconds: number, minutes: number, hours: number, days: number, weeks: number
     millis = ms
-    seconds = Math.round((millis / 1000) % 60)
-    minutes = Math.round((millis / (1000 * 60)) % 60)
-    hours = Math.round((millis / (1000 * 60 * 60)) % 24)
-    days = Math.round(millis / (1000 * 60 * 60 * 24) % 7)
-    weeks = Math.floor(millis / (1000 * 60 * 60 * 24 * 7))
+    seconds = Math.floor((millis / 1000) % 60)
+    minutes = Math.floor((millis / (1000 * 60)) % 60)
+    hours = Math.floor((millis / (1000 * 60 * 60)) % 24)
+    days = Math.floor((millis / (1000 * 60 * 60 * 24)) % 7)
+    weeks = Math.floor((millis / (1000 * 60 * 60 * 24 * 7)))
     if (days < 7) {
         return `${days} Day(s) ${hours} Hour(s) ${minutes} Minute(s) ${seconds} Second(s)`
     } else {
